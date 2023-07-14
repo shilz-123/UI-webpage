ecbc7b2b90beae458fa36e0c8fbd9c6dd3ee38df
Shilpa Kannan <122595633+shilz-123@users.noreply.github.com>
2023-07-14 10:39:11 +0530

    Add files via upload

---
+++ HTML/Javascript/script.js
@@ -0,0 +1,154 @@
+
+                                 // BONCEINUP
+
+        var image = document.getElementById('bounce1');
+        image.classList.add('bounceIn');
+        var image = document.getElementById('bounce2');
+        image.classList.add('bounceIn');
+        var image = document.getElementById('bounce3');
+        image.classList.add('bounceIn');
+        var image = document.getElementById('bounce4');
+        image.classList.add('bounceIn');
+        var image = document.getElementById('bounce5');
+        image.classList.add('bounceIn');
+        var image = document.getElementById('bounce6');
+        image.classList.add('bounceIn');
+        var image = document.getElementById('bounce7');
+        image.classList.add('bounceIn');
+        var image = document.getElementById('bounce8');
+        image.classList.add('bounceIn');
+
+                                // FADEINDOWN
+
+        var image = document.getElementById('fadeInDown1');
+        image.classList.add('fadeInDown')
+
+                                // FADEINLEFT
+
+        var image = document.getElementById('fadeInLeft1');
+        image.classList.add('fadeInLeft')
+        var image = document.getElementById('fadeInLeft2');
+        image.classList.add('fadeInLeft')
+        var image = document.getElementById('fadeInLeft3');
+        image.classList.add('fadeInLeft')
+
+
+                                // FADEINRIGHT
+                
+        var image = document.getElementById('fadeInRight1');
+        image.classList.add('fadeInRight')
+        var image = document.getElementById('fadeInRight2');
+        image.classList.add('fadeInRight')
+                                 // FADEINUP
+    
+        var image = document.getElementById('fadeInUp1');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp2');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp3');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp4');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp5');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp6');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp7');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp8');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp9');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp10');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp11');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp12');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp13');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp14');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp15');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp16');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp17');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp18');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp19');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp20');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp21');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp22');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp23');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp24');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp25');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp26');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp27');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp28');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp29');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp30');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp31');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp32');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp33');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp34');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp35');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp36');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp37');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp38');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp39');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp40');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp41');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp42');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp43');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp44');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp45');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp46');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp47');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp48');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp49');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp50');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp51');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp52');
+        image.classList.add('fadeInUp');
+        var image = document.getElementById('fadeInUp53');
+        image.classList.add('fadeInUp');
+        // var image = document.getElementById('fadeInUp54');
+        // image.classList.add('fadeInUp');
+        // var image = document.getElementById('fadeInUp56');
+        // image.classList.add('fadeInUp');
+    
